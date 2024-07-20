eb8f678a58226ebd48e6b7867092555c6fd98582
“moviedo” <moviedo@konecta-grupo.com>
2024-07-20 14:14:01 -0300

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <div>
+      <div className='flex items-center space-x-2 text-base'>
+        <h4 className='font-semibold text-slate-900'>Contributors</h4>
+        <span className='rounded-full bg-slate-100 px-2 py-1 text-xs font-semibold text-slate-700'>
+          204
+        </span>
+      </div>
+      <div className='mt-3 flex -space-x-2 overflow-hidden'>
+        <img
+          className='inline-block h-12 w-12 rounded-full ring-2 ring-white'
+          src='https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+          alt=''
+        />
+        <img
+          className='inline-block h-12 w-12 rounded-full ring-2 ring-white'
+          src='https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+          alt=''
+        />
+        <img
+          className='inline-block h-12 w-12 rounded-full ring-2 ring-white'
+          src='https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80'
+          alt=''
+        />
+        <img
+          className='inline-block h-12 w-12 rounded-full ring-2 ring-white'
+          src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+          alt=''
+        />
+        <img
+          className='inline-block h-12 w-12 rounded-full ring-2 ring-white'
+          src='https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+          alt=''
+        />
+      </div>
+      <div className='mt-3 text-sm font-medium'>
+        <a href='#' className='text-blue-500'>
+          + 198 others
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
