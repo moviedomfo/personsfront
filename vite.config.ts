188b0b551032eba746af432d448054505a39bc7b
“moviedo” <moviedo@konecta-grupo.com>
2024-07-24 16:01:17 -0300

    f

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personsfront/',
+  base: '/',
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
