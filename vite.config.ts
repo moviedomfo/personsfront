c4a81bf51d54d45eb9848f2a495d653f9e930efd
“moviedo” <moviedo@konecta-grupo.com>
2024-07-24 10:03:08 -0300

    pub

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path';
-
+// https://github.com/moviedomfo/personsfront.git
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://moviedomfo.github.com.io/personsfront',
+
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
